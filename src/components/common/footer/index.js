b8878f59434e5ab210bee54fec49e3070d2c49ce
mahfujul07 <mahfujulhuq07@gmail.com>
2022-08-16 17:47:30 +0400

    init

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="absolute-center max-width">aah finished thanks for visiting</div>
+  );
+};
+
+export default Footer;
